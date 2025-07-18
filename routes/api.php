66f6acff62176af5d3fdc715ce1bf1e02a329c03
Bryan Garcia <garciavaldezbryanuziel04@gmail.com>
2025-07-17 20:00:09 -0600

    CRUD de la tabla de users

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/usuarios', [UserController::class, 'store']);
+Route::delete('/usuarios/{id}', [UserController::class, 'destroy']);
+Route::get('/usuarios/{id}', [UserController::class, 'show']);
+Route::put('/usuarios/{id}', [UserController::class, 'update']);
+
